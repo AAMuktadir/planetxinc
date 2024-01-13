99aafeb4ea3ca0e16998bd54a50bcc1b95ad64af
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-01-13 18:58:57 +0600

    home

--- app/page.js
+++ app/page.js
@@ -7,13 +7,15 @@ export default function Home() {
 
   return (
     <main className="bg-background h-screen pt-40">
-      <div className="text-white text-4xl tracking-widest">
+      <div className="text-white text-8xl tracking-widest pl-20">
         <h2 className="">
-          WE{" "}
-          <span className="border-2 p-1 border-white rounded-full">CREATE</span>
+          WE
+          <span className="border-2 px-4 py-0 border-footer rounded-full text-footer">
+            CREATE
+          </span>
         </h2>
         <h2 className="py-2">DIGITAL</h2>
-        <h2>SOLUTIONS</h2>
+        <h2>_SOLUTIONS</h2>
       </div>
       <div></div>
     </main>
