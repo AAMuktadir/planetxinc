cdb86fe592c0a96c957f69d427e01adccdecc988
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-02-11 18:58:28 +0600

    new marquee

--- Components/home/developmentTools.jsx
+++ Components/home/developmentTools.jsx
@@ -2,78 +2,71 @@ import React from "react";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
 import DevelopmentCard from "../otherComponents/developmentCard";
+import { FaSwift } from "react-icons/fa";
+import { SiKotlin } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { SiRedis } from "react-icons/si";
 
 export default function DevelopmentTools() {
   return (
     <>
       <div className="bg-background py-20">
         <div className="">
-          <Marquee>
-            <div className="flex gap-16 items-center justify-center">
-              <Image
-                src={"/image/icons/mobile.png"}
-                width={150}
-                height={100}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/swift.png"}
-                width={150}
-                height={100}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/kotlin.png"}
-                width={150}
-                height={100}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/react.png"}
-                width={150}
-                height={100}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/java.png"}
-                width={150}
-                height={100}
-                alt="icon"
-              />
+          <Marquee autoFill={true}>
+            <div className="flex gap-32 items-center justify-center text-white text-5xl pr-32 py-2">
+              <div className="text-3xl text-black">
+                <p className="bg-[#5FECC8] px-6 py-2 rounded-full uppercase">
+                  Mobile
+                </p>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <FaSwift />
+                <h4>Swift</h4>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <SiKotlin />
+                <h4>Kotlin</h4>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <FaReact />
+                <h4>React</h4>
+              </div>
+              <div className="flex gap-2 items-center">
+                <FaJava />
+                <h4>Java</h4>
+              </div>
             </div>
           </Marquee>
           <Marquee direction="right" className="pt-20">
-            <div className="flex gap-20 items-center justify-center">
-              <Image
-                src={"/image/icons/web.png"}
-                width={120}
-                height={100}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/js.png"}
-                width={100}
-                height={80}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/nodejs.png"}
-                width={150}
-                height={80}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/html.png"}
-                width={80}
-                height={80}
-                alt="icon"
-              />
-              <Image
-                src={"/image/icons/redis.png"}
-                width={150}
-                height={80}
-                alt="icon"
-              />
+            <div className="flex gap-32 items-center justify-center text-white text-5xl py-2 pr-32">
+              <div className="text-3xl text-black">
+                <p className="bg-[#5FECC8] px-6 py-2 rounded-full uppercase">
+                  Web
+                </p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <SiJavascript />
+                <h4>JavaScript</h4>
+              </div>
+              <div className="flex gap-2 items-center">
+                <FaNode />
+                <h4>Node.js</h4>
+              </div>
+              <div className="flex gap-2 items-center">
+                <FaHtml5 />
+                <h4>Html</h4>
+              </div>
+              <div className="flex gap-2 items-center">
+                <SiRedis />
+                <h4>Redis</h4>
+              </div>
             </div>
           </Marquee>
         </div>
