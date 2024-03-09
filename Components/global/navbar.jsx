b0c826518c0e0cc8b8049ebc7dda8e4b593af616
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-03-09 16:43:51 +0600

    navbar update

--- Components/global/navbar.jsx
+++ Components/global/navbar.jsx
@@ -1,13 +1,7 @@
 "use client";
 import React from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -22,12 +16,45 @@ export default function Navbar() {
           </button>
         </SheetTrigger>
         <SheetContent side={"right"}>
-          <SheetHeader>
-            <SheetTitle className="text-left text-white">Menu</SheetTitle>
-          </SheetHeader>
-          <div className="flex flex-col text-white">
-            <Link href={"/#our-client"} className="">
-              Our client
+          <div className="flex flex-col text-black w-full items-center justify-center h-full  gap-4">
+            <Link
+              href={"/"}
+              className="text-xl sm:text-2xl hover:text-white cursor-pointer"
+            >
+              Home
+            </Link>
+
+            <Link
+              href={"/#our-client"}
+              className="text-xl sm:text-2xl hover:text-white cursor-pointer"
+            >
+              About
+            </Link>
+
+            <Link
+              href={"/#our-client"}
+              className="text-xl sm:text-2xl hover:text-white cursor-pointer"
+            >
+              Products
+            </Link>
+
+            <Link
+              href={"/#our-client"}
+              className="text-xl sm:text-2xl hover:text-white cursor-pointer"
+            >
+              Services
+            </Link>
+            <Link
+              href={"/#our-client"}
+              className="text-xl sm:text-2xl hover:text-white cursor-pointer"
+            >
+              Clients
+            </Link>
+            <Link
+              href={"/#our-client"}
+              className="text-xl sm:text-2xl hover:text-white cursor-pointer"
+            >
+              Contact
             </Link>
           </div>
         </SheetContent>
